18faa3b163acd4fa3dc2cd51d8e863057aba2f5e
jyyblue <jyyblue1987@outlook.com>
2021-02-26 16:12:31 +0800

    [add] read/print data.

--- CipherTest/vigenere.cpp
+++ CipherTest/vigenere.cpp
@@ -1,8 +1,94 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+#define MAX_LEN 512
+#define COL_NUM 80
+
+void readData(char *path, char *data, bool padding) 
+{
+	int cnt = 0;
+	char c = 0;
+	FILE *file = fopen(path, "r");
+	if( file ) 
+	{
+		while ((c = getc(file)) != EOF)
+		{
+			if( 'a' <= c && c <= 'z' || 'A' <= c && c <= 'Z')
+			{
+				if( 'A' <= c && c <= 'Z' )
+					c = c - 'A' + 'a';
+
+				if( cnt >= MAX_LEN )
+					break;
+
+				data[cnt] = c;
+				cnt++;
+			}			
+		}
+
+		fclose(file);
+
+		if( padding == true )
+		{
+			for(; cnt < MAX_LEN; cnt++)
+				data[cnt] = 'x';
+
+			data[MAX_LEN] = NULL;		
+		}
+		else
+		{
+			data[cnt] = NULL;
+		}
+	}
+}
+
+void printData(char *data) 
+{
+	int i = 0;
+	while(true)
+	{
+		char c = data[i];
+		if( c == NULL )
+			break;
+
+		if( i > 0 && i % COL_NUM == 0 )
+			printf("\n");
+
+		printf("%c", c);
+
+		i++;
+	}
+}
 
 int main(int argc,char** argv)
 {
+	if( argc < 3 )
+	{
+		printf("Param is invalid");
+		return -1;
+	}
+
+	char key_data[MAX_LEN + 1];
+	char plain_data[MAX_LEN + 1];
+
+	char *key = argv[1];
+	char *content = argv[2];
+
+	// read key file
+	readData(key, key_data, false);
+
+	// read plain file
+	readData(content, plain_data, true);
+		
+	// print key data
+	printf("\n\n\nVigenere Key:\n\n");
+	printData(key_data);
+
+	// print plain data
+
+	printf("\n\n\nPlaintext:\n\n");
+	printData(plain_data);
+
+
+
 	return 0;
 }
